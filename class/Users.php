77d6a31d5042b6615444087b2e1eeea18928f194
FlavianDelplanque <delplanqueflavian@gmail.com>
2019-08-20 16:02:43 +0200

    function creation compte

--- class/Users.php
+++ class/Users.php
@@ -1,16 +1,28 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+// namespace App;
 /**
  * Description of Users
  *
- * @author ken
+ * @author flavian
  */
 class Users extends Database {
-    //put your code here
+    
+	public function addCompte($nom, $prenom, $adresse, $mail, $password) {
+		$prepare = $this->connect();
+		$add = $prepare->prepare('INSERT INTO users VALUE (null, :nom, :prenom, :adresse, :photo, :statue, :mail, :password, :whishlist)');
+		
+		$add->bindValue('nom', $nom, PDO::PARAM_STR_CHAR);
+		$add->bindValue('prenom', $prenom, PDO::PARAM_STR_CHAR);
+		$add->bindValue('adresse', $adresse, PDO::PARAM_STR_CHAR);
+		$add->bindValue('photo', "lol", PDO::PARAM_STR_CHAR);
+		$add->bindValue('statue', 1, PDO::PARAM_INT);
+		$add->bindValue('mail', $mail, PDO::PARAM_STR_CHAR);
+		$add->bindValue('password', $password, PDO::PARAM_STR_CHAR);
+		$add->bindValue('whishlist', NULL, PDO::PARAM_INT);
+
+		$message = $this->is_ok($add);
+
+		return $message;
+	}
+
 }
