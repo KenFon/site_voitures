2ed0a31cf79d813bda37e052a888fc32e297b977
Ken Fontaine <50627339+KenFon@users.noreply.github.com>
2019-08-21 14:40:54 +0200

    Update addcar.php

--- Elements/handling/addcar.php
+++ Elements/handling/addcar.php
@@ -1,15 +1,31 @@
 <?php
-require_once "../function/function_upload.php";
-
 require_once '../../class/Database.php';
 
 require_once '../../class/Cars.php';
-        
-$para_cars = [$_POST[''], $_POST[''], $_POST[''], $_POST[''], $_POST[''], $_POST[''], $_POST[''], $_POST[''], $_POST[''], $_POST[''], 
-    $_POST[''], $_POST[''], $_POST['']];
+
+require_once '../../function/function_car.php';
+var_dump($_POST);
+
+$para_cars = [
+    $_POST['marque'],
+    $_POST['model'],
+    $_POST['prix'],
+    $_POST['desc'],
+    $_POST['garantie'],
+    $_POST['nbchevaux'],
+    $_POST['km'],
+    $_POST['couleur'],
+    $_POST['type'],
+    $_POST['carburant'],
+    $_POST['annee'],
+    $_FILES['img'],
+];
+
+$car = treatment_cars($para_cars);
+
 
 $cars = new Cars;
-$cars->add_cars($para_cars);
+$cars->add_cars($car);
 
-header('Location:../admin.php');
+header('Location:../../pages/admin.php');
 ?>
