bb2d48a44631a493faf68915a7dc09694ed7fa9e
Ken Fontaine <50627339+KenFon@users.noreply.github.com>
2019-08-21 22:59:11 +0200

    Update Cars.php

--- class/Cars.php
+++ class/Cars.php
@@ -1,61 +1,115 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Cars
  *
  * @author ken
  */
-class Cars extends Database {
+
+class Cars extends Database
+{
 
     /*
      *  @param array
      *  Ajoute une voiture.
+     *  @return bool; TRUE = REQUEST SUCESS, FALSE = REQUEST FAIL
      */
+
     public function add_cars(array $parametre)
     {
         $prepare = $this->connect();
         $add = $prepare->prepare('INSERT INTO voitures VALUE(NULL, :marque, :model, :prix, :description, :garantie, :nbchevaux, :km, :couleur, :type, :carburant, :annee, :slug, :photo');
-       
-       $add->bindValue('marque', $parametre[0], PDO::PARAM_STR_CHAR);
-       $add->bindValue('model', $parametre[1], PDO::PARAM_STR_CHAR);
-       $add->bindValue('prix', $parametre[2], PDO::PARAM_INT);
-       $add->bindValue('description', $parametre[3], PDO::PARAM_STR_CHAR);
-       $add->bindValue('garantie', $parametre[4], PDO::PARAM_INT);
-       $add->bindValue('nbchevaux', $parametre[5], PDO::PARAM_INT);
-       $add->bindValue('km', $parametre[6], PDO::PARAM_INT);
-       $add->bindValue('couleur', $parametre[7], PDO::PARAM_STR_CHAR);
-       $add->bindValue('type', $parametre[8], PDO::PARAM_STR_CHAR);
-       $add->bindValue('carburant', $parametre[9], PDO::PARAM_INT);
-       $add->bindValue('annee', $parametre[10], PDO::PARAM_INT);
-       $add->bindValue('slug', $parametre[11], PDO::PARAM_STR_CHAR);
-       $add->bindValue('photo', $parametre[12], PDO::PARAM_STR_CHAR);
 
-       
-       $message = $this->is_ok($add);
-       
-       return $message;
+        $add->bindValue('marque', $parametre['marque'], PDO::PARAM_STR);
+        $add->bindValue('model', $parametre['model'], PDO::PARAM_STR);
+        $add->bindValue('prix', $parametre['prix'], PDO::PARAM_INT);
+        $add->bindValue('description', $parametre['desc'], PDO::PARAM_STR);
+        $add->bindValue('garantie', $parametre['garantie'], PDO::PARAM_INT);
+        $add->bindValue('nbchevaux', $parametre['nbchevaux'], PDO::PARAM_INT);
+        $add->bindValue('km', $parametre['km'], PDO::PARAM_INT);
+        $add->bindValue('couleur', $parametre['couleur'], PDO::PARAM_STR);
+        $add->bindValue('type', $parametre['type'], PDO::PARAM_STR);
+        $add->bindValue('carburant', $parametre['carburant'], PDO::PARAM_INT);
+        $add->bindValue('annee', $parametre['annee'], PDO::PARAM_INT);
+        $add->bindValue('slug', $parametre['slug'], PDO::PARAM_STR);
+        $add->bindValue('photo', $parametre['img'], PDO::PARAM_STR);
+
+
+        $message = $this->is_ok($add);
+
+        return $message;
     }
-    
+    /*
+     * @param int
+     * recupére l'id et supprime l'élement selectionné
+     */
+
     public function remove_cars($id)
     {
-       $prepare = $this->connect();
-       $remove = $prepare->prepare('DELETE FROM voitures WHERE id=:id');
-       $remove->execute(array(':id' => $id));
+        $prepare = $this->connect();
+        $remove = $prepare->prepare('DELETE FROM voitures WHERE id=:id');
+        $remove->execute(array(':id' => $id));
     }
-    
+
     public function update_cars(array $parametre)
     {
-        
+
     }
-    
-    public function show_cars(array $paremetre=[])
+
+    /*
+     * @param string
+     * Preparation de la requéte SQL pour afficher les resultats en fonction de la recherche
+     */
+
+    private function facile_affich($phrase)
     {
-        
+        $prepare = $this->connect();
+        $show = $prepare->prepare($phrase);
+        $show->execute();
+        $allvoitures = $show->fetchAll(PDO::FETCH_CLASS, "voitures");
+        return $allvoitures;
+    }
+
+    /*
+     * @param array
+     * function à appelé pour afficher les resultats en fonction de la recherche
+     * @return object
+     */
+
+    public function show_cars(array $parametre = [])
+    {
+        $phrase = 'SELECT * FROM voitures';
+
+        if ($parametre == NULL) {
+            $allvoitures = $this->facile_affich($phrase);
+            return $allvoitures;
+        } elseif (count($parametre) > 1) {
+            $phrase = 'SELECT * FROM voitures WHERE ';
+            $part = [];
+
+            foreach ($parametre as $columns => $donnees) {
+
+                if ($columns == 'marque') {
+                    $part['request_marque'] = "marque = $donnees";
+                }
+
+                if ($columns == 'km') {
+                    $part['request_km'] = "km < $donnees";
+                }
+
+                if ($columns == 'couleur') {
+                    $part['request_couleur'] = "couleur = $donnees";
+                }
+
+                if ($columns == 'carbu') {
+                    $part['request_carbu'] = "carbu = $donnees";
+                }
+            }
+
+            $partie_phrase = implode($part, " AND ");
+            $phrase = $phrase . $partie_phrase;
+            $allvoitures = $this->facile_affich($phrase);
+
+            return $allvoitures;
+        }
     }
 }
