c03a7db9efcfa6121d794a9bb5e131cac5855c4d
FlavianDelplanque <42026975+FlavianDelplanque@users.noreply.github.com>
2019-08-22 17:00:08 +0200

    Update creationusers.php

--- Elements/handling/creationusers.php
+++ Elements/handling/creationusers.php
@@ -2,15 +2,28 @@
 
 require_once '../../class/Database.php';
 require_once '../../class/Users.php';
+require_once '../../function/function_img.php';
 
+session_start();
 
 $Nom = htmlspecialchars($_POST['nom']);
 $Prenom = htmlspecialchars($_POST['prenom']);
+$Photo = $_FILES['photo'];
 $Adresse = htmlspecialchars($_POST['adresse']);
 $Mail = htmlspecialchars($_POST['email']);
 $Password = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
 
 
+var_dump($Photo);
+
 $users = new Users;
-$message = $users->addCompte($Nom, $Prenom, $Adresse, $Mail, $Password);
+$emailVerif = $users->infoUsers($Mail);
+if (empty($emailVerif)) {
+	$_SESSION['erreur'] = "email déjà utiliser";
+	header("Location:../../pages/inscription.php");
+}
+else {
+	$Photo = verif_img($Photo, "../../img/photo_profil/");
+	$message = $users->addCompte($Nom, $Prenom, $Adresse, $Photo, $Mail, $Password);
+}
  ?>
