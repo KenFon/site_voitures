c2ebc6f1469eba020fd87eddcc5487a51ad1bd20
ElodieBerquez <51228106+ElodieBerquez@users.noreply.github.com>
2019-08-22 09:34:52 +0200

    Update searchcar.php

--- Elements/handling/searchcar.php
+++ Elements/handling/searchcar.php
@@ -1,1 +1,16 @@
+<?php
 
+  $marques = $_POST['marque'];
+  $km = $_POST['km'];
+  $color = $_POST['couleur'];
+  $carbur = $_POST['carburant'];
+?>
+
+<div class="resultat">
+  <?php
+  // echo $_POST["marque"];
+  // echo $_POST["km"];
+  // echo $_POST["couleur"];
+  // echo $_POST["carburant"];
+   ?>
+</div>
