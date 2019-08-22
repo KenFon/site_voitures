04284f7e57f6251f440b90c786d322f91f6a46cf
ElodieBerquez <51228106+ElodieBerquez@users.noreply.github.com>
2019-08-22 09:36:37 +0200

    Update footer.php

--- Elements/footer.php
+++ Elements/footer.php
@@ -1,8 +1,16 @@
-<?php
+<!-- Je fais une div contenant ma navbar du bas (footer) -->
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<div class="position">
+  <div class="footernav">
+    <div class="adress">
+      <p> 426 rue Gutenberg à Calais</p>
+      <img class="reseau" src="../img/divers/insta.png" alt="icone instagram">
+      <img class="reseau"src="../img/divers/fb.png" alt="icone facebook">
+    </div>
+    <?php echo'<medium>&copy; copyright '.date('Y').'</medium>'; ?>
+  </div>
+</div>
 
+<script src="../javascript/main.js" charset="utf-8"></script>
+</body>
+</html>
