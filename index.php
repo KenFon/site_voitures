91ea8a9502ee1fa6644b3a338c033e50e1199e57
Hamadi88 <51228242+Hamadi88@users.noreply.github.com>
2019-08-22 17:00:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,40 @@
+<?php
+$title='Bienvenue';
+include "Elements/header.php";
+?>
 
+<?php include "Elements/nav.php"?>
+
+
+        <main>
+         <section class="carousel" >
+            <div class="owl-carousel owl-theme">
+               <div style="background-image:url('img/auto1.jpg')">
+                  <a href="" class="t1">Titre 1</a>
+               </div>
+               <div style="background-image:url('img/v2.jpg')">
+                  <a href="" class="t2">Titre 2</a>
+               </div>
+               <div style="background-image:url('img/v3.jpg')">
+                  <a href="" class="t3">Titre 3</a>
+               </div>
+               <div style="background-image:url('img/v4.png')">
+                  <a href="" class="t4">Titre 4</a>
+               </div>
+               <div style="background-image:url('img/v5.png')">
+                  <a href="" class="t5">Titre 5</a>
+               </div>
+               <div style="background-image:url('img/v6.jpg')">
+                  <a href="" class="t6">Titre 6</a>
+               </div>
+               <div style="background-image:url('img/v7.png')">
+                  <a href="" class="t6">Titre 7</a>
+               </div>
+            </div>
+         </section>
+
+         </main>
+
+         
+
+<?php include "Elements/footer.php" ?>
