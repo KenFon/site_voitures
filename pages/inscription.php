5765aae3306c6dc0709ed63f276b3e9a66908a33
FlavianDelplanque <42026975+FlavianDelplanque@users.noreply.github.com>
2019-08-22 17:01:12 +0200

    Update inscription.php

--- pages/inscription.php
+++ pages/inscription.php
@@ -1,8 +1,11 @@
-<form action="../Elements/handling/authentification.php" method="POST" name="inscription">
+<?php session_start(); ?>
+<form action="../Elements/handling/creationusers.php" method="POST" name="inscription" enctype="multipart/form-data">
 	<label>Nom</label>
 	<input type="text" name="nom">
 	<label>Prénom</label>
 	<input type="text" name="prenom">
+	<label>Photo</label>
+	<input type="file" name="photo">
 	<label>Adresse</label>
 	<input type="text" name="adresse">
 	<label>Email</label>
@@ -10,4 +13,5 @@
 	<label>Mot de passe</label>
 	<input type="text" name="mdp">
 	<input type="submit" value="créer un compte" >
+	<p><?php if (isset($_SESSION['erreur'])) { echo $_SESSION['erreur']; } ?></p>
 </form>
