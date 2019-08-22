b376e7c76e2fca8c44a2a874e3ff263eda750285
ElodieBerquez <51228106+ElodieBerquez@users.noreply.github.com>
2019-08-22 16:33:34 +0200

    Update gallerie.php

--- pages/gallerie.php
+++ pages/gallerie.php
@@ -1,8 +1,102 @@
-<?php
+<?php include "../Elements/nav.php"?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+  <main class="row main_galerie">
 
+    <div class="row col m3 s12">
+
+      <div class="col s12">
+        <div class="card">
+          <div class="card-image">
+            <img src="../img/photo_voiture/ford.jpg">
+            <span class="card-title">Ford</span>
+            <a class="btn-floating halfway-fab waves-effect waves-light grey"><i class="material-icons button">+</i></a>
+          </div>
+          <div class="card-content perso">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row col s12 m3">
+      <div class="col s12">
+        <div class="card">
+          <div class="card-image">
+            <img src="../img/photo_voiture/ford.jpg">
+            <span class="card-title ">Ford</span>
+            <a class="btn-floating halfway-fab waves-effect waves-light grey"><i class="material-icons button">+</i></a>
+          </div>
+          <div class="card-content perso">
+            <p>I am a very simple card. I am good at containing small.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row col s12 m3">
+          <div class="col s12">
+            <div class="card">
+              <div class="card-image">
+                <img src="../img/photo_voiture/ford.jpg">
+                <span class="card-title">Card Title</span>
+                <a class="btn-floating halfway-fab waves-effect waves-light grey"><i class="material-icons button">+</i></a>
+              </div>
+              <div class="card-content perso">
+                <p>blahlblkdojgfueh.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row col s12 m3">
+              <div class="col s12">
+                <div class="card">
+                  <div class="card-image">
+                    <img src="../img/photo_voiture/ford.jpg">
+                    <span class="card-title">Card Title</span>
+                    <a class="btn-floating halfway-fab waves-effect waves-light grey"><i class="material-icons button">+</i></a>
+                  </div>
+                  <div class="card-content perso">
+                    <p>blahlblkdojgfueh.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row col s12 m3">
+                  <div class="col s12">
+                    <div class="card">
+                      <div class="card-image">
+                        <img src="../img/photo_voiture/ford.jpg">
+                        <span class="card-title">Card Title</span>
+                        <a class="btn-floating halfway-fab waves-effect waves-light grey"><i class="material-icons button">+</i></a>
+                      </div>
+                      <div class="card-content perso">
+                        <p>blahlblkdojgfueh.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="row col s12 m3">
+                      <div class="col s12">
+                        <div class="card">
+                          <div class="card-image">
+                            <img src="../img/photo_voiture/ford.jpg">
+                            <span class="card-title">Card Title</span>
+                            <a class="btn-floating halfway-fab waves-effect waves-light grey"><i class="material-icons button">+</i></a>
+                          </div>
+                          <div class="card-content perso">
+                            <p>blahlblkdojgfueh.</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+</main>
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems,{});
+});
+
+</script>
+<?php include "../Elements/footer.php"?>
