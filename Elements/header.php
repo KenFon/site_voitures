fb58ff410b20850630e6590fd7b7ba247328f8f9
Ken Fontaine <50627339+KenFon@users.noreply.github.com>
2019-08-22 14:12:54 +0200

    Update header.php

--- Elements/header.php
+++ Elements/header.php
@@ -3,6 +3,8 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
     <title>Occaz'Auto</title>
   </head>
   <body>
