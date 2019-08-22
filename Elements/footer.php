0bf80f74b9968f3229bf5bd52c6ce731005b60fe
Ken Fontaine <50627339+KenFon@users.noreply.github.com>
2019-08-22 14:12:16 +0200

    Update footer.php

--- Elements/footer.php
+++ Elements/footer.php
@@ -12,5 +12,6 @@
 </div>
 
 <script src="../javascript/main.js" charset="utf-8"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 </html>
