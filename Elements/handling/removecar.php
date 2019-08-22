cbab249234c88aceabd9687956742e70c6aa7ca6
FlavianDelplanque <42026975+FlavianDelplanque@users.noreply.github.com>
2019-08-22 13:52:34 +0200

    Update removecar.php

--- Elements/handling/removecar.php
+++ Elements/handling/removecar.php
@@ -1,8 +1,7 @@
 <?php
+include_once '../../class/Cars.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$effacer = new Cars;
+$effacer->remove_cars($_POST['id']);
 
+header('Location:../../pages/admin.php');
