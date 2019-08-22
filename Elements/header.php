59c4b87b21768956cb9990bfb68adbe3dbd76188
Hamadi88 <51228242+Hamadi88@users.noreply.github.com>
2019-08-22 17:00:16 +0200

    Update header.php

--- Elements/header.php
+++ Elements/header.php
@@ -1,10 +1,12 @@
+<?php session_start();?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-    <title>Occaz'Auto</title>
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <title><?=$title?></title>
   </head>
   <body>
