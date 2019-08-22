0d6352f53717a554d8b243dd3f3035072bf1db9f
FlavianDelplanque <42026975+FlavianDelplanque@users.noreply.github.com>
2019-08-22 13:52:56 +0200

    Update Cars.php

--- class/Cars.php
+++ class/Cars.php
@@ -1,24 +1,21 @@
 <?php
+require_once 'Database.php';
 /**
  * Description of Cars
  *
  * @author ken
  */
-
 class Cars extends Database
 {
-
     /*
      *  @param array
      *  Ajoute une voiture.
      *  @return bool; TRUE = REQUEST SUCESS, FALSE = REQUEST FAIL
      */
-
     public function add_cars(array $parametre)
     {
         $prepare = $this->connect();
-        $add = $prepare->prepare('INSERT INTO voitures VALUE(NULL, :marque, :model, :prix, :description, :garantie, :nbchevaux, :km, :couleur, :type, :carburant, :annee, :slug, :photo');
-
+        $add = $prepare->prepare('INSERT INTO voitures VALUE (NULL, :marque, :model, :prix, :description, :garantie, :nbchevaux, :km, :couleur, :type, :carburant, :annee, :slug, :photo)');
         $add->bindValue('marque', $parametre['marque'], PDO::PARAM_STR);
         $add->bindValue('model', $parametre['model'], PDO::PARAM_STR);
         $add->bindValue('prix', $parametre['prix'], PDO::PARAM_INT);
@@ -26,89 +23,73 @@ class Cars extends Database
         $add->bindValue('garantie', $parametre['garantie'], PDO::PARAM_INT);
         $add->bindValue('nbchevaux', $parametre['nbchevaux'], PDO::PARAM_INT);
         $add->bindValue('km', $parametre['km'], PDO::PARAM_INT);
-        $add->bindValue('couleur', $parametre['couleur'], PDO::PARAM_STR);
+        $add->bindValue('couleur', $parametre['color'], PDO::PARAM_STR);
         $add->bindValue('type', $parametre['type'], PDO::PARAM_STR);
         $add->bindValue('carburant', $parametre['carburant'], PDO::PARAM_INT);
         $add->bindValue('annee', $parametre['annee'], PDO::PARAM_INT);
         $add->bindValue('slug', $parametre['slug'], PDO::PARAM_STR);
         $add->bindValue('photo', $parametre['img'], PDO::PARAM_STR);
-
-
-        $message = $this->is_ok($add);
-
-        return $message;
+        $this->is_ok($add);
     }
     /*
      * @param int
      * recupére l'id et supprime l'élement selectionné
      */
-
     public function remove_cars($id)
     {
-        $prepare = $this->connect();
+        $prepare = Self::connect();
         $remove = $prepare->prepare('DELETE FROM voitures WHERE id=:id');
         $remove->execute(array(':id' => $id));
     }
 
     public function update_cars(array $parametre)
     {
-
     }
-
     /*
      * @param string
      * Preparation de la requéte SQL pour afficher les resultats en fonction de la recherche
      */
-
     private function facile_affich($phrase)
     {
         $prepare = $this->connect();
         $show = $prepare->prepare($phrase);
         $show->execute();
-        $allvoitures = $show->fetchAll(PDO::FETCH_CLASS, "voitures");
+        $allvoitures = $show->fetchAll(PDO::FETCH_CLASS);
+
         return $allvoitures;
     }
-
     /*
      * @param array
      * function à appelé pour afficher les resultats en fonction de la recherche
      * @return object
      */
-
     public function show_cars(array $parametre = [])
     {
         $phrase = 'SELECT * FROM voitures';
-
         if ($parametre == NULL) {
             $allvoitures = $this->facile_affich($phrase);
+
             return $allvoitures;
-        } elseif (count($parametre) > 1) {
+        } elseif (count($parametre) > 0) {
             $phrase = 'SELECT * FROM voitures WHERE ';
             $part = [];
-
             foreach ($parametre as $columns => $donnees) {
-
                 if ($columns == 'marque') {
-                    $part['request_marque'] = "marque = $donnees";
+                    $part['request_marque'] = "marque = '$donnees'";
                 }
-
                 if ($columns == 'km') {
-                    $part['request_km'] = "km < $donnees";
+                    $part['request_km'] = "km < '$donnees'";
                 }
-
                 if ($columns == 'couleur') {
-                    $part['request_couleur'] = "couleur = $donnees";
+                    $part['request_couleur'] = "couleur = '$donnees'";
                 }
-
                 if ($columns == 'carbu') {
-                    $part['request_carbu'] = "carbu = $donnees";
+                    $part['request_carbu'] = "carbu = '$donnees'";
                 }
             }
-
             $partie_phrase = implode($part, " AND ");
             $phrase = $phrase . $partie_phrase;
             $allvoitures = $this->facile_affich($phrase);
-
             return $allvoitures;
         }
     }
