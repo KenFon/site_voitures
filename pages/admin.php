5b0c2a2e0eed5ba4a0d3d2780b6707a1bc462dce
Ken Fontaine <50627339+KenFon@users.noreply.github.com>
2019-08-22 11:39:34 +0200

    Update admin.php

--- pages/admin.php
+++ pages/admin.php
@@ -7,21 +7,32 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <title>Document</title>
+    <style>
+        .content{
+            display:none;
+        }
+
+        .content.active{
+            display:block;
+        }
+    </style>
 </head>
+
+
 <body>
 
 <div class="row">
 
     <div class="col s3">
-        <ul>
-            <li><a>Ajouter un véhicule</a></li>
-            <li><a>Modifier un véhicule</a></li>
-            <li><a>Supprimer un véhicule</a></li>
+        <ul class="menu_admin">
+            <li><a class="active" href="#add_car">Ajouter un véhicule</a></li>
+            <li><a href="#update_car">Modifier un véhicule</a></li>
+            <li><a href="#remove_car">Supprimer un véhicule</a></li>
         </ul>
     </div>
 
 
-    <div class="col s8 row add_cars">
+    <div class="col s8 row content active" id="add_car">
         <h1>Ajouter un véhicule</h1>
         <form class="col s12" method="POST" action="../elements/handling/addcar.php" enctype="multipart/form-data">
             <div class="row">
@@ -118,7 +129,7 @@
         </form>
     </div>
 
-    <div class="col s8 row update_car">
+    <div class="col s8 row content" id="update_car">
 
         <h1>Modifier un véhicule</h1>
         <form class="col s12" method="POST" action="../elements/handling/addcar.php" enctype="multipart/form-data">
@@ -217,18 +228,59 @@
         </form>
     </div>
 
-    <div class="col s8 row remove_car">
+    <div class="col s8 row content" id="remove_car">
         <h1>Supprimer un véhicule</h1>
-        afficher la liste de toute les voitures.
+        <div class="row">
+            <?php
+                require_once '../class/Cars.php';
+                $voitures = new Cars;
+                $voitures->show_cars([])
+            ?>
+            <?php foreach ($voitures as $voiture) :?>
+                <div class="col s12 row">
+                    <div class="supp_voiture col s10">
+                        <?= $voiture->marque - $voiture->model - $voiture->prix ?>
+                    </div>
+                    <div class="col s2">
+                        <form method="post" action="../Elements/handling/removecar.php">
+                            <input type="hidden" value="<?= $voiture->id ?>">
+                            <button type="submit">Supprimer</button>
+                        </form>
+
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
 
     </div>
 </div>
 
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         var elems = document.querySelectorAll('select');
         var instances = M.FormSelect.init(elems, {});
     });
+
+    (function(){
+        var menu = document.querySelector('.menu_admin');
+        var as = menu.querySelectorAll('li');
+        as.forEach(li => {
+           li.addEventListener('click', function(e){
+               if(this.classList.contains('active')){
+                   return false;
+               }
+
+               menu.querySelector('li .active').classList.remove('active');
+
+               this.firstChild.classList.add('active');
+
+               document.querySelector('.content.active').classList.remove('active');
+               var attrHref = this.querySelector('a').getAttribute('href').split("#");
+               document.getElementById(attrHref[1]).classList.add('active');
+           })
+        });
+    })();
 </script>
