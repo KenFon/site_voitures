d765c71b9fbeb5acaf1665865dbdeab4f2167382
Ken Fontaine <50627339+KenFon@users.noreply.github.com>
2019-08-23 16:19:55 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -6,4 +6,25 @@ $(document).ready(function(){
                loop: true,
             });
          });
+document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('select');
+        var instances = M.FormSelect.init(elems, {});
+    });
+
+(function(){
+        var menu = document.querySelector('.menu_admin');
+        var as = menu.querySelectorAll('li');
+        as.forEach(li => {
+            li.addEventListener('click', function(e){
+                if(this.classList.contains('active')){
+                    return false;
+                }
+                menu.querySelector('li .active').classList.remove('active');
+                this.firstChild.classList.add('active');
+                document.querySelector('.content.active').classList.remove('active');
+                var attrHref = this.querySelector('a').getAttribute('href').split("#");
+                document.getElementById(attrHref[1]).classList.add('active');
+            })
+        });
+    })();
 
