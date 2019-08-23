f763310e9cde6ea894ded3e43f3f478dc6d2c7c6
Hamadi88 <51228242+Hamadi88@users.noreply.github.com>
2019-08-23 16:00:49 +0200

    Update footer.php

--- Elements/footer.php
+++ Elements/footer.php
@@ -1,4 +1,22 @@
 <!-- Je fais une div contenant ma navbar du bas (footer) -->
+<?php
+    $test2 = $_SERVER["PHP_SELF"];
+
+
+   $cheminjs = explode("/", $test2);
+   
+    
+    if(end($cheminjs)=="index.php")
+              {
+              $cheminjs = "javascript/main.css";
+              }
+            else
+             {
+               $cheminjs = "../javascript/main.js";
+             }
+
+ ?>
+
 
 <div class="position">
   <div class="footernav">
@@ -11,9 +29,20 @@
   </div>
 </div>
 
-<script src="../javascript/main.js" charset="utf-8"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script src="<?= $cheminjs ?>"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
          <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script> 
+ 
+          <script type="text/javascript">
+         $(document).ready(function(){
+            $(".owl-carousel").owlCarousel({
+               items: 1,
+               nav: true,
+               autoplay: true,
+               loop: true,
+            });
+         });
+         </script> 
 </body>
 </html>
+
