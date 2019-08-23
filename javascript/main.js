83fb0952b94bcf24b302304f75df38bfecf92d8d
FlavianDelplanque <42026975+FlavianDelplanque@users.noreply.github.com>
2019-08-23 16:20:20 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -28,3 +28,23 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     })();
 
+(function(){
+	console.log("test");
+    var menu = document.querySelector('.menu_profil');
+    var as = menu.querySelectorAll('li');
+    as.forEach(li => {
+       li.addEventListener('click', function(e){
+           if(this.classList.contains('active')){
+               return false;
+           }
+
+           menu.querySelector('li .active').classList.remove('active');
+
+           this.firstChild.classList.add('active');
+
+           document.querySelector('.content.active').classList.remove('active');
+           var attrHref = this.querySelector('a').getAttribute('href').split("#");
+           document.getElementById(attrHref[1]).classList.add('active');
+       })
+    });
+})(); 
