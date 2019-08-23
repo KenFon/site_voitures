6624c8f78b0b2459ac3a01571ecb9bbb076a5124
Hamadi88 <51228242+Hamadi88@users.noreply.github.com>
2019-08-23 15:57:57 +0200

    Update header.php

--- Elements/header.php
+++ Elements/header.php
@@ -1,12 +1,35 @@
 <?php session_start();?>
+
+ <?php
+    $voitures = new Cars;
+
+    $test1 = $_SERVER["PHP_SELF"];
+
+  
+   $chemin = explode("/", $test1);
+   echo end($chemin);
+    
+    if(end($chemin)=="index.php")
+              {
+              $chemin = "css/main.css";
+              }
+            else
+             {
+               $chemin = "../css/main.css";
+             }
+
+ ?>
+
 <!DOCTYPE html>
-<html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="../css/main.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?= $chemin ?>" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
     <title><?=$title?></title>
-  </head>
-  <body>
+    
+</head>
+<body>
