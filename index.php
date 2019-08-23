0e08726c2d6a3dbfbc6bcdf0cff3031a90959321
Hamadi88 <51228242+Hamadi88@users.noreply.github.com>
2019-08-23 16:01:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 $title='Bienvenue';
+require_once 'class/Cars.php';
 include "Elements/header.php";
+$test = $voitures->show_cars();
+//voiture->photo ça me donne le chemin de la photo correspondant a la voiture.
+//voiture->marque ça me donne la marque de la voiture correspondant.
 ?>
 
 <?php include "Elements/nav.php"?>
@@ -9,27 +13,11 @@ include "Elements/header.php";
         <main>
          <section class="carousel" >
             <div class="owl-carousel owl-theme">
-               <div style="background-image:url('img/auto1.jpg')">
-                  <a href="" class="t1">Titre 1</a>
-               </div>
-               <div style="background-image:url('img/v2.jpg')">
-                  <a href="" class="t2">Titre 2</a>
-               </div>
-               <div style="background-image:url('img/v3.jpg')">
-                  <a href="" class="t3">Titre 3</a>
-               </div>
-               <div style="background-image:url('img/v4.png')">
-                  <a href="" class="t4">Titre 4</a>
-               </div>
-               <div style="background-image:url('img/v5.png')">
-                  <a href="" class="t5">Titre 5</a>
-               </div>
-               <div style="background-image:url('img/v6.jpg')">
-                  <a href="" class="t6">Titre 6</a>
-               </div>
-               <div style="background-image:url('img/v7.png')">
-                  <a href="" class="t6">Titre 7</a>
-               </div>
+               <?php foreach ($test as $voiture) :?>
+                  <div style="background-image:url(<?=$voiture->photo?>)">
+                     <a href="" class="t1"><?=$voiture->marque ?></a>
+                  </div>
+               <?php endforeach ?>
             </div>
          </section>
 
